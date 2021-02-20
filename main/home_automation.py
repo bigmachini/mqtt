fb697d8e6b2675d516ed834124de0f01683af333
ewaudo <ewaudo@copiakenya.com>
2021-02-20 13:19:07 +0300

    fix attribureError on pin_no

--- main/home_automation.py
+++ main/home_automation.py
@@ -1,19 +1,20 @@
 from time import sleep
 from main.relay import RelayManager
 
-relays = [
-    {'pin_no': 12, "pin_type": "out", "name": "pin_12"},
-    {'pin_no': 14, "pin_type": "out", "name": "pin_14"},
-    {'pin_no': 27, "pin_type": "out", "name": "pin_27"},
-]
-
-relay_manager = RelayManager(relays)
-
 
 def start():
+    relays = [
+        {'pin_no': 12, "pin_type": "out", "name": "pin_12"},
+        {'pin_no': 14, "pin_type": "out", "name": "pin_14"},
+        {'pin_no': 27, "pin_type": "out", "name": "pin_27"}
+    ]
+    relay_manager = RelayManager(relays)
     while True:
         for _ in relays:
-            relay = relay_manager.get_relay_by_pin(_.pin_no)
+            _pin_no = _.get('pin_no', None)
+            print('_pin_no', _pin_no)
+            #TODO: Add functionality to check for None
+            relay = relay_manager.get_relay_by_pin(_pin_no)
             print(relay.name, relay.pin_no, relay.pin_type)
             relay.update_state(0)
             sleep(5)
