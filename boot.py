d98d601359389ac7709ff099ea823b7fd53ed478
ewaudo <ewaudo@copiakenya.com>
2021-02-24 18:09:46 +0300

    Refactor code:

--- boot.py
+++ boot.py
@@ -1,7 +1,8 @@
 import esp
 import network
-from machine import RTC
+from machine import RTC, reset
 import main.secrets as secret
+import time
 
 esp.osdebug(None)
 import gc
@@ -15,6 +16,17 @@ station = network.WLAN(network.STA_IF)
 station.active(True)
 station.connect(ssid, password)
 
+count = 0
+print("connecting to wifi: ")
+while not station.isconnected():
+
+    print(".", end="")
+    time.sleep(1)
+    count = count + 1
+    # TODO make this a configuration
+    if count >= 120:
+        reset()
+
 if station.isconnected():
     import ntptime
 
@@ -22,3 +34,4 @@ if station.isconnected():
 
 print('Connection successful at {}'.format(RTC().datetime()))
 print(station.ifconfig())
+
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import _thread
+from machine import reset
+from time import sleep
 
 
 def connect_to_wifi_and_update():
-    import time, machine, network, gc
+    import time, network, gc
     time.sleep(1)
     print('Memory free', gc.mem_free())
 
@@ -17,11 +19,11 @@ def connect_to_wifi_and_update():
         sta_if.connect(secret.WIFI_SSID, secret.WIFI_PASSWORD)
         while not sta_if.isconnected():
             print(".", end="")
-            time.sleep(1)
+            sleep(1)
             count = count + 1
-            #TODO make this a configuration
+            # TODO make this a configuration
             if count >= 120:
-                machine.reset()
+                reset()
     else:
         import ntptime
         ntptime.settime()
@@ -30,7 +32,7 @@ def connect_to_wifi_and_update():
     otaUpdater = OTAUpdater(secret.GITHUB_URL, main_dir='main', secrets_file="secrets.py")
     hasUpdated = otaUpdater.install_update_if_available()
     if hasUpdated:
-        machine.reset()
+        reset()
     else:
         del (otaUpdater)
         gc.collect()
@@ -39,20 +41,17 @@ def connect_to_wifi_and_update():
 def start_app():
     # TODO: Add functionality to increase sleep time and sevearity of
     # situation when exceptions occur
-    import machine
     try:
         import main.main as main
         main.start(connect_to_wifi_and_update)
     except Exception as ex:
         print('start_app:: ex --> ', ex)
-        machine.reset()
+        reset()
 
 
 try:
     connect_to_wifi_and_update()
     _thread.start_new_thread(start_app, ())
 except Exception as ex:
-    import machine
-
     print('main:: ex --> ', ex)
-    machine.reset()
+    reset()
